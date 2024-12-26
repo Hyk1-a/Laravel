02f2f5571b74f6e747a3858b1c26dd19b3c3b099
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-12-26 14:26:59 +0630

    commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -14,8 +14,7 @@
         <nav class="container ">
             <div class="flex justify-between items-center" >
                     <button id="sidebarToggle" class="px-2 py-2 hover:bg-slate-500 rounded-lg text-white transition duration-300 ease-in-out transform hover:scale-105">
-                        <ion-icon name="menu-outline" class="hidden md:block" size="large"></ion-icon>
-                        <ion-icon name="menu-outline" class="block md:hidden" size="small"></ion-icon>
+                        <ion-icon name="menu-outline" size="small"></ion-icon>
                     </button>
             </div>
             <div class="flex justify-center items-center">
